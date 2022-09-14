1b27c2ce0dea4a1a0195c119b5ef019760a84982
Bharath-SR <bharath.s0941@gmail.com>
2022-09-14 21:27:02 +0530

    Started: Address Book Problem

---
+++ AddressBook/src/com/bridgeLabz/AddressBook.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.bridgeLabz;
+
+/**
+ * @author Admin
+ *
+ */
+public class AddressBook {
+
+	/**
+	 *Address Book ability to create Contacts
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
